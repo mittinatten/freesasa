cb63a5fbf09681018f34469c7b8f8c9039a0924d
Simon Mitternacht <simon.mitternacht@ub.uib.no>
2013-06-13 16:14:47 +0200

    Added help message to main application.

--- calc_sasa.c
+++ calc_sasa.c
@@ -11,6 +11,17 @@
 extern int getopt(int, char * const *, const char *);
 #endif
 
+void help(const char* optarg) {
+    fprintf(stderr,"\nUsage: %s [options] [< pdb-file]\n",optarg);
+    fprintf(stderr,"\nOptions are:\n"
+	           "       -h  print this message\n"
+                   "       -f  pdb-file\n"
+	           "       -n  number of test points in Shrake & Rupley algorithm\n"
+	           "           Default is %d, max value is %d.\n"
+	    ,DEF_SR_POINTS,MAX_SR_POINTS);
+    fprintf(stderr,"\nIf no pdb-file is specified STDIN is used for input.\n\n");
+}
+
 int main (int argc, char **argv) {
     int n_sr_points = DEF_SR_POINTS;
     protein *p; 
@@ -20,21 +31,26 @@ int main (int argc, char **argv) {
     extern char *optarg;
     char opt;
 
-    while ((opt = getopt(argc, argv, "f:n:")) != -1) {
+    while ((opt = getopt(argc, argv, "f:n:h")) != -1) {
         switch(opt) {
+	case 'h':
+	    help(argv[0]);
+	    exit(0);
 	case 'f':
 	    input = fopen(optarg, "r");
 	    if (input == NULL) {
-		fprintf(stderr,"\nError: could not open file '%s'.\n", 
+		fprintf(stderr,"\nError: could not open file '%s'.\n\n", 
 			optarg);
+		help(argv[0]);
 		exit(1);
 	    }
 	    break;
 	case 'n':
 	    n_sr_points = atoi(optarg);
 	    if (n_sr_points > MAX_SR_POINTS) {
-		fprintf(stderr,"\nError: n = %d is too large (%d is limit).\n",
+		fprintf(stderr,"\nError: n = %d is too large (%d is limit).\n\n",
 			n_sr_points, MAX_SR_POINTS);
+		help(optarg);
 		exit(1);
 	    }
 	    break;
