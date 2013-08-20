69e0652d20312e5d9e70127eefa875f7fc202bd1
Simon Mitternacht <simon.mitternacht@ub.uib.no>
2013-08-20 16:11:21 +0200

    Refined analysis of accuracy. Made plot more transparent using quantile boxes.

--- analysis/precision.pl
+++ analysis/precision.pl
@@ -1,6 +1,7 @@
 
 use strict;
 use warnings;
+use Statistics::Descriptive;
 
 my @ref_files = `ls reference/*`;
 my @sr_files = `ls SR/*/*`;
@@ -9,22 +10,54 @@ my @lr_files = `ls LR/*/*`;
 my %ref_data;
 my %sr_data;
 my %lr_data;
-
+#my %sum;
+my %val;
 
 read_file(\%ref_data,$_) for @ref_files;
 read_file(\%sr_data,$_)  for @sr_files;
 read_file(\%lr_data,$_)  for @lr_files;
 
 for my $protein (keys %ref_data) {
+    get_val("SR",$protein,%sr_data);
+    get_val("LR",$protein,%lr_data);
+}
+
+
+for (keys %val) {
+    #print (join "\n", @{$val{$_}{t}});
+    print "STAT $_ ";
+    print_quantile (@{$val{$_}{t}});
+    print_quantile (@{$val{$_}{d}});
+    print "\n";
+}
+
+sub print_quantile {
+    my $stat = Statistics::Descriptive::Full->new();
+    $stat->add_data(@_);
+    
+    # this avoids a strange bug in percentile(), don't understand it fully yet
+    my ($x1, $idx1) = $stat->percentile(5); 
+    
+    printf("%6f %6f %6f %6f %6f ", 
+	   $stat->quantile(2), $stat->quantile(1), 
+	   $stat->quantile(3), ($stat->get_data)[$idx1],
+	   $stat->percentile(95));
+    $stat->clear();
+}
+
+
+sub get_val {
+    my ($name,$protein,%data) = @_;
     my $r = $ref_data{$protein}[0]->{total};
     my $n = $ref_data{$protein}[0]->{n_atoms};
-    for (sort { $a->{par} <=> $b->{par} } @{$sr_data{$protein}}) {
-	print "SR $protein $n $r ";
-	print "$_->{par} $_->{total} $_->{t}\n";
-    }
-    for (sort { $a->{par} <=> $b->{par} } @{$lr_data{$protein}}) {
-	print "LR $protein $n $r ";
-	print "$_->{par} $_->{total} $_->{t}\n";
+    for (@{$data{$protein}}) {
+	#print "$name $protein $n $r ";
+	#print "$_->{par} $_->{total} $t $d\n";
+	my $key = "$name\_$_->{par}";
+	my $t = $_->{t};
+	my $d = abs($r-$_->{total})/$n;
+	push @{$val{$key}{t}}, $t/$n;
+	push @{$val{$key}{d}}, $d;
     }
 }
 
@@ -49,5 +82,6 @@ sub read_file
     }
     $pdbfile =~ s/.*\/(\w\w\w\w).pdb/$1/;
     push @{$data->{$pdbfile}}, \%tmp;
+
 }
 
