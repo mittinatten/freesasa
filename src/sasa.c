2e9122acbab1a8869a9a9bf7397d652dd6a9ab96
Simon Mitternacht <simon.mitternacht@ub.uib.no>
2013-08-28 10:08:05 +0200

    Speed improvements for Shrake & Rupley, by precalculating as many values as possible.

--- src/sasa.c
+++ src/sasa.c
@@ -24,6 +24,11 @@
 #include "sasa.h"
 #include "srp.h"
 
+/** internal functions for S&R calculations **/
+double sasa_shrake_rupley_calc_atom(int i, const vector3 *xyz, const double *r, 
+				    int n_atoms, int n_points, const vector3 *srp_v3,
+				    const char *spcount_0);
+
 /** internal functions for L&R calculations **/
 void sasa_add_slice_area(double *sasa, double z, double delta, const vector3 *xyz, 
 			 const double *radii, const int **nb, const int *nn, 
@@ -51,65 +56,76 @@ void sasa_shrake_rupley(double *sasa,
 {
     const double *srp = srp_get_points(n_points);
 
-    //turn test-points into vector3's
+    //turn test-points into vector3's of unit length
     vector3 srp_v3[n_points];
     for (int k = 0; k < n_points; ++k) {
         vector3_set(&srp_v3[k],srp[k*3],srp[k*3+1],srp[k*3+2]);
         vector3_setlength(&srp_v3[k],1.0); //just to be sure
     }
-    vector3 test;
+
+    /* a reference zero array is prepared so that it won't need to be
+       initialized for each atom */
+    char spcount_0[n_points];
+    for (int k = 0; k < n_points; ++k) {
+	spcount_0[k] = 0;
+    }
 
     //calculate SASA
     for (int i = 0; i < n_atoms; ++i) {
-
-        /* this array keeps track of which testpoints belonging to
-           a certain atom overlap with other atoms */
-        char spcount[n_points];
-        for (int k = 0; k < n_points; ++k) {
-            spcount[k] = 0;
-        }
-
-        double ri = r[i]+PROBE_RADIUS;
-        for (int j = 0; j < n_atoms; ++j) {
-            if (j == i) continue;
-            const double rj = r[j]+PROBE_RADIUS;
-            const double cut2 = (ri+rj)*(ri+rj);
-            if (vector3_dist2(&xyz[i],&xyz[j]) > cut2)
-                continue;
-            for (int k = 0; k < n_points; ++k) {
-                if (spcount[k]) continue;
-
-                /* this probably cannot be done beforehand, because
-                   changing the length repeatedly could cause
-                   round-off errors */
-                vector3_copy(&test, &srp_v3[k]);
-
-                // test-point vectors have length 1.0
-                vector3_multiply(&test, ri);
-                vector3_add(&test, &xyz[i]);
-
-                if (vector3_dist2(&test, &xyz[j]) <= rj*rj) {
-                    spcount[k] = 1;
-                }
-                /* i.e. if |xyz[i]+ri*srp[k] - xyz[j]| <= rj we have an
-                   overlap. */
-            }
-        }
-        int n_surface = 0;
-        for (int k = 0; k < n_points; ++k) {
-            if (!spcount[k]) ++n_surface;
-#ifdef DEBUG
-            if (!spcount[k]) {
-                vector3_copy(&test,&srp_v3[k]);
-                vector3_multiply(&test,ri);
-                vector3_add(&test,&xyz[i]);
-                printf("%f %f %f\n",test.x,test.y,test.z);
-            }
-#endif
-        }
-        //paranthesis to make sure floating point division is used (I am paranoid about this).
-        sasa[i] = (4.0*PI*ri*ri*n_surface)/n_points;
+	sasa[i] = sasa_shrake_rupley_calc_atom(i,xyz,r,n_atoms,n_points,
+					       srp_v3,spcount_0);
     }
+}
+
+double sasa_shrake_rupley_calc_atom(int i, const vector3 *xyz, const double *r, 
+				    int n_atoms, int n_points, const vector3 *srp_v3_ref, 
+				    const char *spcount_0) {
+    /* this array keeps track of which testpoints belonging to
+       a certain atom overlap with other atoms */
+    char spcount[n_points];
+    double ri = r[i]+PROBE_RADIUS;
+    /* testpoints for this atom */
+    vector3 srp_v3_ri[n_points];
+    
+    memcpy(spcount,spcount_0,sizeof(char)*n_points);
+
+    /* Initialize testpoints for this atom*/
+    memcpy(srp_v3_ri,srp_v3_ref,sizeof(vector3)*n_points);
+    for (int j = 0; j < n_points; ++j) {
+	//set length (the reference vectors should have length 1.0)
+	vector3_multiply(&srp_v3_ri[j],ri);
+	//translate vectors to surround correct atom
+	vector3_add(&srp_v3_ri[j],&xyz[i]);
+    }
+    
+    for (int j = 0; j < n_atoms; ++j) {
+	if (j == i) continue;
+	const double rj = r[j]+PROBE_RADIUS;
+	const double cut2 = (ri+rj)*(ri+rj);
+	if (vector3_dist2(&xyz[i],&xyz[j]) > cut2)
+	    continue;
+	for (int k = 0; k < n_points; ++k) {
+	    if (spcount[k]) continue;
+	    if (vector3_dist2(&srp_v3_ri[k], &xyz[j]) <= rj*rj) {
+		spcount[k] = 1;
+	    }
+	    /* i.e. if |xyz[i]+ri*srp[k] - xyz[j]| <= rj we have an
+	       overlap. */
+	}
+    }
+    int n_surface = 0;
+    for (int k = 0; k < n_points; ++k) {
+	if (!spcount[k]) ++n_surface;
+#ifdef DEBUG
+	if (!spcount[k]) {
+	    vector3_copy(&test,&srp_v3[k]);
+	    vector3_multiply(&test,ri);
+	    vector3_add(&test,&xyz[i]);
+	    printf("%f %f %f\n",test.x,test.y,test.z);
+	}
+#endif
+    }
+    return (4.0*PI*ri*ri*n_surface)/n_points;
 }
 
 void sasa_lee_richards(double *sasa,
