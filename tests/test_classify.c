5807608bb66b34c20579b386f65a33da524536c1
Simon Mitternacht <simon.mitternacht@gmail.com>
2014-02-15 17:10:58 +0100

    Fixed buffer overflow in classify-tests. Added two more amino acids.

--- tests/test_classify.c
+++ tests/test_classify.c
@@ -9,38 +9,38 @@
 struct atom {
     const char *a;
     const char *b;
-    double radius;
+    const double radius;
 };
-const double ali_C = 2.00, aro_C = 1.75, car_O = 1.40, car_C = 1.55, ami_N = 1.55;
-const int n_atom_types = 15; //size of previous array
+#define ali_C 2.00
+#define aro_C 1.75
+#define car_O 1.40
+#define car_C 1.55
+#define ami_N 1.55
+#define n_atom_types 31 //size of array below
 
 const struct atom atoms[] = {
-    {"ALA"," C  ",car_C},
-    {"ALA"," O  ",car_O},
-    {"ALA"," CA ",ali_C},
-    {"ALA"," N  ",ami_N},
-    {"ALA"," CB ",ali_C},
-    {"ARG"," C  ",car_C},
-    {"ARG"," O  ",car_O},
-    {"ARG"," CA ",ali_C},
-    {"ARG"," N  ",ami_N},
-    {"ARG"," CB ",ali_C},
-    {"ARG"," CG ",ali_C},
-    {"ARG"," CD ",ali_C},
-    {"ARG"," CZ ",ali_C},
-    {"ARG"," NE ",ami_N},
-    {"ARG"," NH1",ami_N},
-    {"ARG"," NH2",ami_N}
+    {"ALA"," C  ",car_C},    {"ALA"," O  ",car_O},    {"ALA"," CA ",ali_C},
+    {"ALA"," N  ",ami_N},    {"ALA"," CB ",ali_C},
+    {"ARG"," C  ",car_C},    {"ARG"," O  ",car_O},    {"ARG"," CA ",ali_C},
+    {"ARG"," N  ",ami_N},    {"ARG"," CB ",ali_C},    {"ARG"," CG ",ali_C},
+    {"ARG"," CD ",ali_C},    {"ARG"," CZ ",ali_C},    {"ARG"," NE ",ami_N},
+    {"ARG"," NH1",ami_N},    {"ARG"," NH2",ami_N},
+    {"ASN"," C  ",car_C},    {"ASN"," O  ",car_O},    {"ASN"," CA ",ali_C},
+    {"ASN"," N  ",ami_N},    {"ASN"," CB ",ali_C},    {"ASN"," CG ",ali_C},
+    {"ASN"," OD1",car_O},    {"ASN"," ND2",ami_N},
+    {"ASP"," C  ",car_C},    {"ASP"," O  ",car_O},    {"ASP"," CA ",ali_C},
+    {"ASP"," N  ",ami_N},    {"ASP"," CB ",ali_C},    {"ASP"," CG ",ali_C},
+    {"ASN"," OD1",car_O},    {"ASN"," OD2",car_O},
 };
 
 START_TEST (test_radius)
 {
-    char buf[20];
+    char buf[50];
     for (int i = 0; i < n_atom_types; ++i) {
-	struct atom a = atoms[i];
-	double ret = sasalib_classify_radius(a.a,a.b);
-	sprintf(buf,"%s %s ret=%f ref=%f",a.a,a.b,ret,a.radius);
-	ck_assert_msg(fabs(ret - a.radius) < 1e-10,buf);
+        const struct atom a = atoms[i];
+        double ret = sasalib_classify_radius(a.a,a.b);
+        sprintf(buf,"%s %s ret=%f ref=%f",a.a,a.b,ret,a.radius);
+        ck_assert_msg(fabs(ret - a.radius) < 1e-10,buf);
     }
 }
 END_TEST
