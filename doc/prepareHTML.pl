4be12360106831e216d78b93eccadeb7803570aa
Simon Mitternacht <simon.mitternacht@gmail.com>
2015-09-22 17:50:59 +0200

    Fixed a few minor things in the HTML-manual.

--- doc/prepareHTML.pl
+++ doc/prepareHTML.pl
@@ -1,6 +1,10 @@
 #!/usr/bin/perl
 use strict;
-use XML::DOM;
+
+# This is a total hack, but ran into problems with MathML using
+# HTML::DOM and problems with HTML5 using XML::DOM. Since it's just a
+# few simple things that needs to be done on one particular file, this
+# should do
 
 while (<>) {
     if (/<\/head>/) {
@@ -8,9 +12,10 @@ while (<>) {
         print '<link href="stylesheets/github-light.css" rel="stylesheet" type="text/css">'."\n";
         print '<link href="stylesheets/article.css" rel="stylesheet" type="text/css">'."\n";
     }
+    $_ =~ s/<nav .*>/<header>/;
+    $_ =~ s/<\/nav>/<\/header>/;
     $_ =~ s/style=".*"/style=""/;
     $_ =~ s/ltx_page_main/ltx_page_main wrapper/;
-    $_ =~ s/--/&en;/;
     print;
 }
 exit;
