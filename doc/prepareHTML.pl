9ffb646ec47c1ed1342adde1ae6b354b667f56c2
Simon Mitternacht <simon.mitternacht@gmail.com>
2015-09-23 17:04:47 +0200

    Worked on text to make it more web-friendly and added some JS to get tracking and MathJax.

--- doc/prepareHTML.pl
+++ doc/prepareHTML.pl
@@ -6,12 +6,42 @@ use strict;
 # few simple things that needs to be done on one particular file, this
 # should do
 
+my $tracking = <<HERE;
+<script type="text/javascript"
+  src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
+</script>
+<script type="text/x-mathjax-config">
+MathJax.Hub.Config({
+  MathML: {
+    useMathMLspacing: false
+  }
+});
+MathJax.Hub.Config({
+  "HTML-CSS": {
+    availableFonts: ["Latin-Modern"],
+    webFont: "Latin-Modern",
+    scale: 90
+  }
+});
+</script>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+     ga('create', 'UA-66397346-1', 'auto');
+     ga('send', 'pageview');
+</script>
+HERE
+
 while (<>) {
     if (/<\/head>/) {
         print '<link href="stylesheets/styles.css" rel="stylesheet" type="text/css">'."\n";
         print '<link href="stylesheets/github-light.css" rel="stylesheet" type="text/css">'."\n";
         print '<link href="stylesheets/article.css" rel="stylesheet" type="text/css">'."\n";
     }
+    print $tracking if (/<\/body>/);
     $_ =~ s/<nav .*>/<header>/;
     $_ =~ s/<\/nav>/<\/header>/;
     $_ =~ s/style=".*"/style=""/;
