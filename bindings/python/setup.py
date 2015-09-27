1d76cadadff56d8af180b63a59bc70b9fdf47f89
Simon Mitternacht <simon.mitternacht@gmail.com>
2015-09-27 23:02:53 +0200

    added some installation info to the python bindings

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -12,5 +12,11 @@ extensions = [
 ]
 
 setup(
+    name='FreeSASA',
+    description='Calculate solvent accessible surface areas of proteins',
+    version='0.5.0', # how to make this use the version of the rest of the library
+    author='Simon Mitternacht',
+    url='http://mittinatten.github.io/freesasa/',
+    license='GPL-v3',
     ext_modules = cythonize(extensions)
 )
