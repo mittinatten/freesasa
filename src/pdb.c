dd6944b35db05daa81449b6889a670d5e6a9f166
Arghya Chakravorty <arghyac@g.clemson.edu>
2017-11-08 11:25:49 -0500

    Corrected freesasa_pdb_ishydrogen function
    This function previously identified some non-hydrogen atoms as hydrogens if the second character in the atom name was 'H/D'. Example: CD,CD1,CD2 or OD1, OD2 etc. Due to this the number of non-H atoms being read were incorrect. 
    
    A new line in the function has been added (defined in pdb.c)

--- src/pdb.c
+++ src/pdb.c
@@ -149,9 +149,11 @@ freesasa_pdb_get_atom_name(char *name,
     assert(line);
     if (pdb_line_check(line,PDB_ATOM_NAME_STRL+12) == FREESASA_FAIL) {
         name[0] = '\0';
+        printf("Failed : %s\n", line);
         return FREESASA_FAIL;
     }
     strncpy(name,line+12,PDB_ATOM_NAME_STRL);
+    printf("%s\n", name );
     name[PDB_ATOM_NAME_STRL] = '\0';
     return FREESASA_SUCCESS;
 }
@@ -265,10 +267,13 @@ freesasa_pdb_ishydrogen(const char* line)
 {
     assert(line);
     if (pdb_line_check(line,13) == FREESASA_FAIL) return FREESASA_FAIL;
+    //false positive hydrogen
+    if (line[12] != 'H' && (line[13] == 'H' ||  line[13] == 'D')) return 0;
     //hydrogen
     if (line[12] == 'H' || line[13] == 'H') return 1;
     //hydrogen
     if (line[12] == 'D' || line[13] == 'D') return 1;
+
     return 0;
 }
 
